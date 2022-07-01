f7bbe48e68d207b1fef7440ab0a8b340d7c3eb7e
RomanNesterenko <roman.nesterenko@siz.bz>
2022-07-01 12:57:42 +0300

    sad

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-let max_num = Number.NEGATIVE_INFINITY;
-console.log(max_num + 1);
+function rotateAndRemove(arr) {
+  const arrRes = [];
+  
+  function fn1(params) {
+    console.log(params);
+    const arrReduce = params.reduce((acc, el1, i) => {
+      acc.push(arr[i].shift());
+      return acc;
+    }, []);
+    arrRes.push(arrReduce);
+    if (arr[0].length) {
+      fn1(arr);
+    }
+  }
+  fn1(arr);
+
+  function fn2(params) {
+    const arrReduce = params.reduce((acc, el) => {
+      const arr = [...el];
+      const sortArr = arr.sort((a, b) => a - b);
+      el.splice(el.indexOf(sortArr[0]), 1);
+      el.splice(el.indexOf(sortArr[sortArr.length - 1]), 1);
+
+      acc.push(el);
+
+      return acc;
+    }, []);
+   
+    fn1(arrReduce);
+  }
+  console.log(arrRes);
+  fn2(arrRes);
+}
+
+console.log(
+  rotateAndRemove([
+    [3, 5, 8, 4, 2],
+    [1, 9, 2, 3, 8],
+    [4, 6, 7, 2, 2],
+    [7, 5, 5, 5, 5],
+    [6, 5, 3, 8, 1],
+  ])
+);
